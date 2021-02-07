7b2b38b6ddfee70dfb90657d5871f6f4a154dd89
Neale Johnston <neale@team-johnston.com>
2021-02-07 14:24:29 +0000

    add region

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -4,23 +4,26 @@ import { Icon } from "leaflet";
 import "../styles/map.css";
 
 const MapComponent = ({ castlesList }) => {
-  const [activeCastle, setActiveCastle] = useState();
-
   return (
     <MapContainer center={[56.799, -4.298]} zoom={7}>
-      {castlesList.map((castle) => (
-        <Marker
-          key={castle._id}
-          position={[castle.coordinates[0], castle.coordinates[2] * -1]}
-        >
-          <Popup>
-            <div>
-              <h2>{castle.name}</h2>
-              <a href={castle.link}>Wikipedia</a>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
+      {castlesList.map((castle) => {
+        if (castle.coordinates.length !== 0) {
+          return (
+            <Marker
+              key={castle._id}
+              position={[castle.coordinates[0], castle.coordinates[2] * -1]}
+            >
+              <Popup>
+                <div>
+                  <h2>{castle.name}</h2>
+                  <h4>Region: {castle.region}</h4>
+                  <a href={castle.link}>Wikipedia</a>
+                </div>
+              </Popup>
+            </Marker>
+          );
+        } else return null;
+      })}
 
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
